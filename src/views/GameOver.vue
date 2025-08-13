1035508ecb46f0409a0119e2732e8dd5d39149d7
Giovani Sales <giovanisales14@gmail.com>
2025-08-13 20:06:52 -0300

    cria pagina de game over.

---
+++ src/views/GameOver.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="menu-bg">
+        <div class="container">
+            <div class="d-flex justify-content-center align-items-center flex-column">
+                <img src="../assets/game-over.png" alt="" tabindex="-1" class="game-over-img">
+                <b-button 
+                    variant="danger"
+                    class="btn-home"
+                    @click="redirectMenu"
+                >
+                    Tentar novamente
+                </b-button>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data()  {
+        
+    },
+    
+    mounted(){
+      console.log(this.$route)
+    },
+    methods:{
+        redirectMenu(){
+            this.$router.push('/')
+        }
+    },
+}
+</script>
+<style>
+.game-over-img{
+    width: 60% !important;
+    height: 60%;
+    pointer-events: none;
+}
+*:focus {
+  caret-color: transparent !important; /* esconde o cursor */
+  outline: none !important; /* esconde a borda de foco */
+}
+</style>
