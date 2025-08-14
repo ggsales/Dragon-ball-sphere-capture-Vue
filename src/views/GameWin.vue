1414e20f699c1ef00709f35b757e28a97e7b3d3a
Giovani Sales <giovanisales14@gmail.com>
2025-08-13 21:59:25 -0300

    inicia nova view para game vencido.

---
+++ src/views/GameWin.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="menu-bg">
+        <div class="container">
+            <div class="d-flex justify-content-center align-items-center flex-column">
+                <img src="../assets/game-win.png" alt="" tabindex="-1" class="game-over-img">
+                <b-button 
+                    variant="danger"
+                    class="btn-home"
+                    @click="redirectMenu"
+                >
+                    Reiniciar
+                </b-button>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data()  {
+        
+    },
+    
+    mounted(){
+    },
+    methods:{
+        redirectMenu(){
+            this.$router.push('/')
+        }
+    },
+}
+</script>
+<style>
+.game-over-img{
+    width: 60% !important;
+    height: 60%;
+    pointer-events: none;
+}
+*:focus {
+  caret-color: transparent !important; /* esconde o cursor */
+  outline: none !important; /* esconde a borda de foco */
+}
+</style>
