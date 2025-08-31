460e7ed9122b9484c034a8c7c915b14057f2b229
Giovani Sales <giovanisales14@gmail.com>
2025-08-31 15:27:44 -0300

    corrige renderização aleatoria das esferas e aplica nova lógica para subtração de chances no jogo.

--- src/views/mainGame.vue
+++ src/views/mainGame.vue
@@ -1,100 +1,227 @@
 <template>
   <div class="menu-bg">
-    <h1>A dificuldade é 
-      {{dificuldade}}
-    </h1>
+    <h5>A dificuldade é {{ dificuldade }}</h5>
+
     <div class="cronometro-box">
-      <div v-for="(esfera) in esferas" :key="esfera.esferaAtual">
-        <img :src="getImagePath(esfera.img)" :alt="'Esfera ' + esfera.esferaAtual">
-      </div>
+      Tempo restante: <span>{{ tempo }}</span>
     </div>
 
+    <div class="vidas">
+      <img 
+        v-for="(vida, index) in vidas" 
+        :key="index"
+        :src="getImagePath(vida ? 'esfera-cheia.png' : 'esfera-vazia.png')"
+        class="vida"
+      />
+    </div>
+
+    <div class="jogo-area">
+      <img
+        v-for="(esfera, index) in esferas"
+        :key="esfera.id"
+        :src="getImagePath('esfera.png')"
+        :class="[esfera.tamanho, esfera.lado]"
+        :style="{ left: esfera.x + 'px', top: esfera.y + 'px', position: 'absolute' }"
+        @click="clicarEsfera(index)"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-export default{
-  data(){
-    return{
+export default {
+  data() {
+    return {
+      largura: 0,
       altura: 0,
-      largura : 0,
-      vida: 1,
       tempo: 15,
       tempoEsfera: 1500,
-      dificuldade: this.$route.params.dificuldade,
-      esferas : [
-        {
-          img:"esfera-cheia.png",
-          esferaAtual: 1,
-
-        },
-         {
-          img:"esfera-cheia.png",
-          esferaAtual: 2
-        },
-        {
-          img:"esfera-cheia.png",
-          esferaAtual: 3
-        }
-      ]
+      vidas: [true, true, true],
+      esferas: [],
+      timerCronometro: null,
+      timerEsferas: null,
     }
   },
-    mounted() {
-      this.tempoDeJogo()
-      this.removeEsfera()
+
+  computed:{
+    dificuldade() {
+      return this.$route.params.dificuldade
+  }
+
+  }, 
+  mounted() {
+    this.ajustaTamanhoPalcoJogo()
+    this.definirDificuldade()
+    this.iniciaCronometro()
+    this.iniciaEsferas()
+  },
+  beforeUnmount() {
+    clearInterval(this.timerCronometro)
+    clearInterval(this.timerEsferas)
+  },
+  methods: {
+    getImagePath(imageName) {
+      return require(`@/assets/${imageName}`)
     },
 
-    
-    methods:{
-      getImagePath(imageName) {
-        return require(`@/assets/${imageName}`);
-      },
+    ajustaTamanhoPalcoJogo() {
+      this.altura = window.innerHeight
+      this.largura = window.innerWidth
+    },
 
+    definirDificuldade() {
+      if (this.dificuldade === "normal") this.tempoEsfera = 1500
+      else if (this.dificuldade === "dificil") this.tempoEsfera = 1000
+      else if (this.dificuldade === "maisDeOitoMil") this.tempoEsfera = 850
+    },
 
-      tempoDeJogo(){
-        if(this.dificuldade === "normal"){
-          this.tempoEsfera = 1500
-        } else if (this.dificuldade === "dificil"){
-          this.tempoEsfera = 1000
-        } else if(this.dificuldade === "maisDeOitoMil"){
-          this.tempoEsfera = 850
+    iniciaCronometro() {
+      this.timerCronometro = setInterval(() => {
+        this.tempo--
+        if (this.tempo < 0) {
+          clearInterval(this.timerCronometro)
+          clearInterval(this.timerEsferas)
+          this.verificaFimDeJogo(true) 
         }
-      },
+      }, 1000)
+    },
 
-      removeEsfera(index){
-        this.timer = setInterval(() => {
+    iniciaEsferas() {
+      this.timerEsferas = setInterval(() => {
+        this.geraEsfera()
+      }, this.tempoEsfera)
+    },
+
+    geraEsfera() {
+      const posicaoX = Math.max(Math.floor(Math.random() * this.largura) - 90, 0)
+      const posicaoY = Math.max(Math.floor(Math.random() * this.altura) - 90, 0)
+
+      const esfera = {
+        id: Date.now(),
+        x: posicaoX,
+        y: posicaoY,
+        tamanho: this.tamanhoAleatorio(),
+        lado: this.ladoAleatorio(),
+        ativa: true,
+      }
+
+      this.esferas.push(esfera)
+
+      
+      setTimeout(() => {
+        const index = this.esferas.findIndex(e => e.id === esfera.id)
+        if (index !== -1 && this.esferas[index].ativa) {
           this.esferas.splice(index, 1)
-          if(this.esferas.length === 0){
-            clearInterval(this.timer)
-            this.$router.push({
+          this.perdeVida()
+        }
+      }, this.tempoEsfera - 100) 
+    },
+
+    clicarEsfera(index) {
+      this.esferas.splice(index, 1)
+    },
+
+    perdeVida() {
+      const idx = this.vidas.findIndex(v => v === true)
+      if (idx !== -1) this.$set(this.vidas, idx, false)
+
+      if (!this.vidas.includes(true)) {
+        clearInterval(this.timerCronometro)
+        clearInterval(this.timerEsferas)
+        this.verificaFimDeJogo(false) 
+      }
+    },
+
+    verificaFimDeJogo(venceu) {
+      if (venceu && this.vidas.includes(true)) {
+        this.$router.push({
+              path: '/game-win',
+              query: {
+                gameWin: true
+              } 
+        })
+      } else {
+        this.$router.push({
               path: '/game-over',
               query: {
                 gameWin: false
               } 
-            })
-          }
-        }, 1000)  
-        
+        })
       }
+    },
 
-    }
+    tamanhoAleatorio() {
+      const classes = ['esfera1', 'esfera2', 'esfera3']
+      return classes[Math.floor(Math.random() * classes.length)]
+    },
+
+    ladoAleatorio() {
+      return Math.random() > 0.5 ? 'ladoA' : 'ladoB'
+    },
+  }
 }
 </script>
 
 <style scoped>
-.menu-bg{
-  position: relative;
+
+
+.menu-bg {
+    background-image: url(../assets/bg.png);
+    width: 100%;
+    height: 100vh;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    cursor: url('../assets/goku1.png') 30 30, auto;
 }
-.cronometro-box{
+
+.cronometro-box {
   position: absolute;
+  top: 10px;
+  left: 10px;
   background: rgba(0,0,0,0.7);
-  width: 300px;
-  height: 80px;
-  bottom: 0;
-  opacity: 2;
-  color: aliceblue;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  color: white;
+  padding: 10px;
+  border-radius: 8px;
 }
+
+.vidas {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  display: flex;
+  gap: 5px;
+}
+
+.vida {
+  width: 40px;
+}
+
+.jogo-area img {
+  cursor: pointer;
+}
+
+.esfera1 {
+  width: 50px;
+  height: 50px;
+}
+
+.esfera2 {
+  width: 80px;
+  height: 80px;
+}
+
+.esfera3 {
+  width: 120px;
+  height: 120px;
+}
+
+.ladoA {
+  transform: scaleX(1);
+}
+
+.ladoB {
+  transform: scaleX(-1);
+}
+
 </style>
